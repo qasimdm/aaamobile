3f782b4901693b55f8c638ce2ec4f931425cd614
qasim <qasimdm@hotmail.com>
2016-11-17 16:06:37 +0100

    17/11 final commit

--- app/src/main/java/app15/aaamobile/view/CardPayment.java
+++ app/src/main/java/app15/aaamobile/view/CardPayment.java
@@ -1,6 +1,7 @@
 package app15.aaamobile.view;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.TextWatcher;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,13 +20,17 @@ import com.stripe.android.model.Token;
 import com.stripe.exception.AuthenticationException;
 
 import app15.aaamobile.R;
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CardPayment extends Fragment {
+    private final int MY_SCAN_REQUEST_CODE = 0;
 
     private AutoCompleteTextView tvCardNumber, tvCardExpMonth, tvCardExpYear, tvCardCVC;
+
     public CardPayment() {
         // Required empty public constructor
     }
@@ -40,7 +46,14 @@ public class CardPayment extends Fragment {
         tvCardExpYear = (AutoCompleteTextView)view.findViewById(R.id.et_card_exp_year);
         tvCardCVC = (AutoCompleteTextView)view.findViewById(R.id.et_card_cvc);
 
+        Button btnCardScan = (Button)view.findViewById(R.id.btn_io_read_card);
         Button btnCardPay = (Button)view.findViewById(R.id.btn_make_card_payment);
+        btnCardScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onScanPress(getView());
+            }
+        });
         btnCardPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -80,6 +93,58 @@ public class CardPayment extends Fragment {
             }
         });
         return view;
+    }
+    public void onScanPress(View v) {
+        Intent scanIntent = new Intent(getContext(), CardIOActivity.class);
+
+        // customize these values to suit your needs.
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, true); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false); // default: false
+
+        // MY_SCAN_REQUEST_CODE is arbitrary and is only used within this activity.
+        startActivityForResult(scanIntent, MY_SCAN_REQUEST_CODE);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_SCAN_REQUEST_CODE) {
+            String resultDisplayStr;
+            if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
+                CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+
+                // Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
+                resultDisplayStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+                Log.i("CardPayment", resultDisplayStr);
+
+                // Do something with the raw number, e.g.:
+                // myService.setCardNumber( scanResult.cardNumber );
+
+                if (scanResult.isExpiryValid()) {
+                    resultDisplayStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n";
+                    Log.i("CardPayment", resultDisplayStr);
+                }
+
+                if (scanResult.cvv != null) {
+                    // Never log or display a CVV
+                    resultDisplayStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+                    Log.i("CardPayment", resultDisplayStr);
+                }
+
+                if (scanResult.postalCode != null) {
+                    resultDisplayStr += "Postal Code: " + scanResult.postalCode + "\n";
+                    Log.i("CardPayment", resultDisplayStr);
+                }
+            }
+            else {
+                resultDisplayStr = "Scan was canceled.";
+                Log.i("CardPayment", resultDisplayStr);
+            }
+            // do something with resultDisplayStr, maybe display it in a textView
+            // resultTextView.setText(resultDisplayStr);
+        }
+        // else handle other activity results
     }
 
 }
